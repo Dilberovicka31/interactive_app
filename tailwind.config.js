0043aade1a46af299c4d8605fcee26403ca56e8b
Mija Dilberovic <mijadilberovic@Mijas-MacBook-Pro.local>
2024-03-26 13:48:16 -0700

    added tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './src/**/*.{html,js,jsx,ts,tsx,vue, handlebars}',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
